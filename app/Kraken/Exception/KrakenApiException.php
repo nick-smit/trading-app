50e0361aed3254316dec282a84847c1fb4c2de81
Nick Smit <nicksmit1994@gmail.com>
2021-07-07 08:13:14 +0200

    Remove PHP App

--- app/Kraken/Exception/KrakenApiException.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace App\Kraken\Exception;
-
-
-class KrakenApiException extends \Exception
-{
-}
--- config/kraken.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'api_key' => env('KRAKEN_API_KEY'),
-    'private_key' => env('KRAKEN_PRIVATE_KEY')
-];
