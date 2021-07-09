c4eb618176c8865d3718e5a2537ac2e4c8f8fd81
Nick Smit <nicksmit1994@gmail.com>
2021-07-09 17:33:46 +0200

    Fix only checking BTC bug

--- bot_long_short_sma_crossing.py
+++ bot_long_short_sma_crossing.py
@@ -4,8 +4,12 @@ from helpers import log
 import strategy_long_short_sma_crossing
 from bot_lib import *
 
-def check_buy_or_sell_signal(market: dict, df: pd.DataFrame):
+def check_buy_or_sell_signal(market: dict, df: pd.DataFrame, executeOrders):
     decision = strategy_long_short_sma_crossing.make_decision(market['in_position'], df)
+
+    if not executeOrders:
+        print("{}: {}".format(market['symbol'], decision))
+        return
 
     if decision == 'buy':
         market['in_position'] = buy(market['symbol'], market['max_decimals'])
@@ -14,12 +18,10 @@ def check_buy_or_sell_signal(market: dict, df: pd.DataFrame):
 
 markets = getMarkets()
 
-def bot_long_short_sma_crossing():
+def bot_long_short_sma_crossing(executeOrders=True):
     log("Checking markets", False)
     for i in range(len(markets)):
         market = markets[i]
-        if (market['symbol'] != 'BTC/EUR'):
-            continue
 
         candles = None
         try:
@@ -30,4 +32,7 @@ def bot_long_short_sma_crossing():
 
         df = strategy_long_short_sma_crossing.calculate(candles)
 
-        check_buy_or_sell_signal(markets[i], df)
+        check_buy_or_sell_signal(markets[i], df, executeOrders)
+
+if __name__ == '__main__':
+    bot_long_short_sma_crossing(False)
